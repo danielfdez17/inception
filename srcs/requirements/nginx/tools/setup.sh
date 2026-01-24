31a1ae8e03743475a8b5bc01ea4d9c283549f614
danielfdez17 <daniel17dev@gmail.com>
2026-01-24 19:55:56 +0100

    feat: nginx only

---
+++ srcs/requirements/nginx/tools/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# ? -nodes -> evita passphrase
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/C=ES/ST=Madrid/L=Madrid/O=42/OU=Inception/CN=localhost"
+
+# ? Para que el contenedor no muera
+nginx -g "daemon off;"
