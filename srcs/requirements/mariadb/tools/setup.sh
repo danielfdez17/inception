50121d7f004bd61b11273a2fadd979a7761cb9ae
danielfdez17 <daniel17dev@gmail.com>
2026-01-24 21:06:54 +0100

    fix: mariadb service

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -5,18 +5,19 @@ if [ ! -d /var/lib/mysql/mysql ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 fi
 
-mysqld_safe &
+mysqld_safe --skip-networking &
 
 sleep 5
 
-mysql -u root << EOF
+mysql << EOF
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
 
-mysqladmin shutdown
+mysqladmin shutdown -p"${MYSQL_ROOT_PASSWORD}"
 
 exec mysqld_safe
