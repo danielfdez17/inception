474a8e818c56d6a55d086d8d3327dcbd6e609f63
danielfdez17 <daniel17dev@gmail.com>
2026-01-25 11:44:17 +0100

    fix: add wordpress users

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -9,6 +9,21 @@ if [ ! -f wp-config.php ]; then
 	cp /wp-config.php wp-config.php
 fi
 
+wp core install \
+	--url=danfern3.42.fr \
+	--title="Inception" \
+	--admin_user=danfern3 \
+	--admin_password=inceptionPassword \
+	--admin_email=danfern3@student.42.fr \
+	--skip-email \
+	--allow-root
+
+wp user create editor_user editor@42.fr \
+	--role=editor \
+	--user_pass=editorpass \
+	--allow-root
+
+
 mkdir -p /run/php
 
 exec php-fpm83 -F
