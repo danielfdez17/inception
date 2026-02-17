8ca9e2500b546bd8d1475dc7e71058112c73b77f
danielfdez17 <daniel17dev@gmail.com>
2026-02-17 20:00:10 +0100

    testing

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -6,29 +6,37 @@ mkdir -p /var/lib/mysql /var/run/mysqld
 chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
 
 if [ ! -d /var/lib/mysql/mysql ]; then
+    echo "Initializing MariaDB data directory..."
     mysql_install_db --user=mysql --ldata=/var/lib/mysql
+
+    # Empieza el servicio de MariaDB en segundo plano temporalmente
+    # para ejecutar comandos de configuración inicial
+    mysqld_safe --nowatch &
+
+    # Espera a que el servidor esté listo
+    until mariadb-admin ping --silent; do
+        echo "Waiting for MariaDB server to start..."
+        sleep 1
+    done
+
+    echo "Setting root password and creating database..."
+
+    # Se configura la contraseña de root y se crea la base de datos
+    mariadb -u root <<EOSQL
+        ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';
+        CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';
+        GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+
+        CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};
+        CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';
+        GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO '${MARIADB_USER}'@'%';
+        FLUSH PRIVILEGES;
+EOSQL
+
+    # Detiene el servidor MariaDB temporalmente iniciado
+    mysqladmin -u root -p${MARIADB_ROOT_PASSWORD} shutdown
+    echo "MariaDB initialization complete."
 fi
 
-mysqld_safe &
-
-until mariadb-admin ping --silent; do
-	echo "Waiting for MariaDB server to start..."
-	sleep 1
-done
-
-mariadb -u root << EOF
-FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
-
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-GRANT PROCESS ON *.* TO '${MYSQL_USER}'@'%';
-FLUSH PRIVILEGES;
-EOF
-
-mysqladmin -u root shutdown -p"${MYSQL_ROOT_PASSWORD}"
-
+echo "Starting MariaDB server..."
 exec mysqld_safe
